3ad0e772efb5a5663fc3a346377cf5783b6b072b
jiin <jiinshin@dyn-129-236-218-40.dyn.columbia.edu>
2016-11-06 17:24:44 -0500

    Round 5
    Added pseudocode to code.js
    Refined index.html
    Refined style.css

---
+++ pivoting/code.js
@@ -0,0 +1,49 @@
+// **'Show Me An Example!' Section**
+
+// Multiple Choice Question
+
+// User uses dropdown menu to select an answer
+// User clicks submit which triggers function
+  // If/else statement to trigger one of two responses:
+    // A1 and A3 (value: incorrect)
+      // Grey box becomes Red box
+      // "Incorrect." Appears
+      // Default explanation appears
+
+      // A2 (value: correct)
+      // Grey box becomes Green box
+      // "Correct!" Appears
+      // Default explanation appears
+
+
+// **'Break It Down For Me!' Section**
+
+// Interactive Flowchart of Study
+
+  // User clicks 'Healthcare: Related' option
+    // Healthcare button turns darker green
+    // Drug Use button color fades
+    // War on Terror button color fades
+
+      // **"active" state of third column buttons:
+        // background-color: #5c5c5c (dark grey)
+        // text-color: white
+
+    // "Recalled" div becomes "active"
+    //  "Trustworthy, honest, likable" div becomes active
+
+  // User clicks 'Drug Use: Somewhat Related' option
+    // Healthcare button color fades
+    // Drug Use button turns darker orange
+    // War on Terror button color fades
+
+    // "Did not recall" div becomes "active"
+    //  "Trustworthy, honest, likable" div becomes active
+
+  // User clicks 'War on Terror: Not At All Related' option
+    // Healthcare button color fades
+    // Drug Use button color fades
+    // War on Terror button turns darker red
+
+    // "Recalled" div becomes "active"
+    //  "Untrustworthy, dishonest, unlikable" div becomes active
